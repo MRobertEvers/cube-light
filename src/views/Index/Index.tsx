e08ba0c4f6644d022cdb1a98449a0c81a51fdaf0
Matthew Evers <mreverssci@gmail.com>
2020-09-07 18:55:38 -0500

    feat: base table

--- src/views/Index/Index.tsx
+++ src/views/Index/Index.tsx
@@ -1,10 +1,35 @@
 import { NextPage } from 'next';
 import { Page } from '../../components/Page/Page';
 
+import styles from './index.module.css';
+
 export type WorkoutProps = {};
 
+const tmp = [];
+for (let i = 0; i < 100; i += 1) {
+	tmp.push('Words');
+}
+
 const Index: NextPage<WorkoutProps> = (props: WorkoutProps) => {
-	return <Page>Home Page</Page>;
+	return (
+		<Page>
+			<div className={styles['index-container']}>
+				<div>
+					<div className={styles['avatar']}></div>
+				</div>
+				<table className={styles['decklist']}>
+					<tr>
+						<th>Card Name</th>
+						<th>#</th>
+					</tr>
+					<tr>
+						<td>Shark Typhoon</td>
+						<td>1</td>
+					</tr>
+				</table>
+			</div>
+		</Page>
+	);
 };
 
 export default Index;
