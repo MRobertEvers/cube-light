894b963f1af0ce6bf6a888a8214dd78b7a7b30f6
Matthew Evers <mreverssci@gmail.com>
2020-09-07 17:23:39 -0500

    feat: home page baseline

---
+++ src/views/Index/Index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+
+export type WorkoutProps = {};
+
+const Workout: NextPage<WorkoutProps> = (props: WorkoutProps) => {
+	return <div>Home Page</div>;
+};
+
+export default Workout;
