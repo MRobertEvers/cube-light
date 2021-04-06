cad8161e0d99580438f64d73c36a06ec08bb171c
Matthew Evers <mreverssci@gmail.com>
2021-04-05 21:05:55 -0500

    feat: create reducer manager typed

--- projects/clientv2/src/store/configure-store.ts
+++ projects/clientv2/src/store/configure-store.ts
@@ -1,16 +1,32 @@
-import { configureStore as configureStoreToolkit, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
+import {
+	configureStore as configureStoreToolkit,
+	EnhancedStore,
+	getDefaultMiddleware
+} from '@reduxjs/toolkit';
+import { combineReducers, ReducersMapObject } from 'redux';
 import { cardNameLookupSlice } from './card-name-lookup/card-name-lookup.state';
+import { createReducerManager, ReducerManager } from './create-reducer-manager';
 
 import type { RootState } from './root-state.types';
 
-const rootReducer = combineReducers<RootState>({
+const rootReducer: ReducersMapObject<RootState> = {
 	[cardNameLookupSlice.name]: cardNameLookupSlice.reducer
-});
+};
 
-export function configureStore() {
-	return configureStoreToolkit({
-		reducer: rootReducer,
+export type StoreType = EnhancedStore<RootState, any> & {
+	reducerManager: ReducerManager<RootState>;
+};
+
+export function configureStore(): StoreType {
+	const reducerManager = createReducerManager(rootReducer);
+
+	const store = configureStoreToolkit({
+		reducer: reducerManager.reduce,
 		middleware: getDefaultMiddleware()
 	});
+
+	return {
+		...store,
+		reducerManager: reducerManager
+	};
 }
